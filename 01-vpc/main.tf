1ee51ce3b47486e74768bcf718a5f41d370cb30b
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-04-03 11:26:17 +0530

    Update main.tf

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "main" {
     Name = "terraform-vpc"
   }
 }
-
+ 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"

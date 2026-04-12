607d5e6db30840689165bd836ab4984023a882de
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-04-12 10:13:41 +0530

    Update ec2.tf

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "roboshop" {
+resource "aws_instance" "roboshop" { 
   ami           = var.ami_id
   instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
   vpc_security_group_ids = [ aws_security_group.allow_all.id] 

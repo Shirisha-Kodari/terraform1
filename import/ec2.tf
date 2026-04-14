456968570380c68a9a9a435b739d1c1e227c7cca
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-04-14 09:40:06 +0530

    Update ec2.tf

--- import/ec2.tf
+++ import/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "linux" { 
+resource "aws_instance" "linux" {  
   ami = "ami-09c813fb71547fc4f" 
   instance_type = "t3.micro" 
   vpc_security_group_ids = ["sg-03f077f464f434b47"]

6dfd41892e56c64a7005f8adda70231b44d3690a
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-04-15 07:53:54 +0530

    Update ec2.tf

--- terraform-multi-env/workspaces/ec2.tf
+++ terraform-multi-env/workspaces/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "roboshop" {
   count                  = length(var.instances)
   ami                    = var.ami_id # left and right side names no need to be same
   instance_type          = lookup(var.instance_type, terraform.workspace)
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  vpc_security_group_ids = [aws_security_group.allow_all.id] 
  
   tags = merge( 
     var.common_tags,
